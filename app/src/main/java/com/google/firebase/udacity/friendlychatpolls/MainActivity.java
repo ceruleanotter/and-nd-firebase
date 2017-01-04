92cdebdb3e2a3246377bc694ee01dca9d720b74a
Lyla <lyla@udacity.com>
2017-01-03 22:19:48 -0800

    Fix: Remove remote config, cap number messages, fix bug with pictures names/url switched

--- app/src/main/java/com/google/firebase/udacity/friendlychatpolls/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychatpolls/MainActivity.java
@@ -23,7 +23,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
-import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
@@ -37,34 +36,21 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity implements MessageAdapter.VoteOnClickHandler {
 
     private static final String TAG = "MainActivity";
-
-    //public static final String ANONYMOUS = "anonymous";
-    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
-    public static final String FRIENDLY_MSG_LENGTH_KEY = "friendly_msg_length";
 
     public static final int RC_SIGN_IN = 1;
     private static final int RC_PHOTO_PICKER = 2;
@@ -87,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private FirebaseStorage mFirebaseStorage;
     private StorageReference mChatPhotosStorageReference;
-    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseStorage = FirebaseStorage.getInstance();
-        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
         mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
         mChatPhotosStorageReference = mFirebaseStorage.getReference().child("chat_photos");
@@ -148,7 +133,6 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
             public void afterTextChanged(Editable editable) {
             }
         });
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
 
         // Send button sends a message and clears the EditText
         mSendButton.setOnClickListener(new View.OnClickListener() {
@@ -192,22 +176,6 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
                 }
             }
         };
-
-        // Create Remote Config Setting to enable developer mode.
-        // Fetching configs from the server is normally limited to 5 requests per hour.
-        // Enabling developer mode allows many more requests to be made per hour, so developers
-        // can test different config values during development.
-        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
-                .setDeveloperModeEnabled(BuildConfig.DEBUG)
-                .build();
-        mFirebaseRemoteConfig.setConfigSettings(configSettings);
-
-        // Define default config values. Defaults are used when fetched config values are not
-        // available. Eg: if an error occurred fetching values from the server.
-        Map<String, Object> defaultConfigMap = new HashMap<>();
-        defaultConfigMap.put(FRIENDLY_MSG_LENGTH_KEY, DEFAULT_MSG_LENGTH_LIMIT);
-        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
-        fetchConfig();
     }
 
     @Override
@@ -236,7 +204,7 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
                             Uri downloadUrl = taskSnapshot.getDownloadUrl();
 
                             // Set the download URL to the message box, so that the user can send it to the database
-                            FriendlyMessage friendlyMessage = new FriendlyMessage(null, mUser.getDisplayName(), downloadUrl.toString());
+                            FriendlyMessage friendlyMessage = new FriendlyMessage(null, downloadUrl.toString(), mUser.getDisplayName());
                             mMessagesDatabaseReference.push().setValue(friendlyMessage);
                         }
                     });
@@ -289,7 +257,8 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
 
     private void attachRecyclerViewAdapter() {
         // Initialize message RecyclerView and its adapter
-        mMessageAdapter = new MessageAdapter(FriendlyMessage.class, R.layout.item_message, MessageAdapter.FriendlyMessageHolder.class, mMessagesDatabaseReference, this);
+        mMessageAdapter = new MessageAdapter(FriendlyMessage.class, R.layout.item_message,
+                MessageAdapter.FriendlyMessageHolder.class, mMessagesDatabaseReference.limitToLast(50), this);
 
 
         // Scroll to bottom on new messages
@@ -302,51 +271,6 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
 
 
         mMessageRecyclerView.setAdapter(mMessageAdapter);
-    }
-
-
-    // Fetch the config to determine the allowed length of messages.
-    public void fetchConfig() {
-        long cacheExpiration = 3600; // 1 hour in seconds
-        // If developer mode is enabled reduce cacheExpiration to 0 so that each fetch goes to the
-        // server. This should not be used in release builds.
-        if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
-            cacheExpiration = 0;
-        }
-        mFirebaseRemoteConfig.fetch(cacheExpiration)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // Make the fetched config available
-                        // via FirebaseRemoteConfig get<type> calls, e.g., getLong, getString.
-                        mFirebaseRemoteConfig.activateFetched();
-
-                        // Update the EditText length limit with
-                        // the newly retrieved values from Remote Config.
-                        applyRetrievedLengthLimit();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // An error occurred when fetching the config.
-                        Log.w(TAG, "Error fetching config", e);
-
-                        // Update the EditText length limit with
-                        // the newly retrieved values from Remote Config.
-                        applyRetrievedLengthLimit();
-                    }
-                });
-    }
-
-    /**
-     * Apply retrieved length limit to edit text field. This result may be fresh from the server or it may be from
-     * cached values.
-     */
-    private void applyRetrievedLengthLimit() {
-        Long friendly_msg_length = mFirebaseRemoteConfig.getLong(FRIENDLY_MSG_LENGTH_KEY);
-        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(friendly_msg_length.intValue())});
-        Log.d(TAG, FRIENDLY_MSG_LENGTH_KEY + " = " + friendly_msg_length);
     }
 
 
@@ -371,11 +295,7 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.Vo
             }
         });
 
-
         Log.e("MainActivity", "voted " + voteRef.toString());
-
-
-
 
     }
 }
